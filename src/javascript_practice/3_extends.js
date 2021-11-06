1ec413df7a31e2f7bd9a88fa11590a248930ed73
Gabrielle Ford <gabrielledeva@gmail.com>
2021-11-06 15:18:05 -0400

    finished 3_extends

---
+++ src/javascript_practice/3_extends.js
@@ -0,0 +1,45 @@
+// ES6 JS Classes
+class User {
+  constructor(name) {
+    this.name = name;
+    this.type = "Trial User";
+  };
+
+  // Method 1
+  greet() {
+    console.log(`Welcome back, ${this.name}`);
+  };
+
+  // Method 2
+  status() {
+    console.log(`Current status: ${this.type}`);
+  };
+};
+
+class TrialUser extends User {
+    trialEnding() {
+        console.log(`Your trial will be ending soon, ${this.name}. Would you like to join our program?`);
+    };
+};
+
+class BannedUser extends User {
+    banReason() {
+        console.log(`smh - You were yeeted for being a jerk, ${this.name}.`);
+    }
+}
+
+// Instance of User class
+let anonDude = new User('Anonymous');
+anonDude.greet();
+anonDude.status();
+
+// Instance of TrialUser class
+let trialUser = new TrialUser('Paul');
+trialUser.greet();
+trialUser.trialEnding();
+trialUser.status();
+
+// Instance of BannedUser
+let bannedUser = new BannedUser('Jake');
+bannedUser.greet();
+bannedUser.banReason();
