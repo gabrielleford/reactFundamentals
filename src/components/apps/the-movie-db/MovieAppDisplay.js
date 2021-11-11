9511dc6d58ec4578edbd41dcea1df202b53e76f1
Gabrielle Ford <gabrielledeva@gmail.com>
2021-11-11 11:58:55 -0500

    finished 9.2 - need to start challenge

--- src/components/apps/the-movie-db/MovieAppDisplay.js
+++ src/components/apps/the-movie-db/MovieAppDisplay.js
@@ -1,12 +1,41 @@
 import React from "react";
+import styled from "styled-components";
 
 const MovieAppDisplay = ({movie}) => {
+    const Movie = styled.div`
+    margin: 5px;
+    width: 300px;
+    height: 500px;
+    border-radius: 5px;
+    text-align: center;
+    background: #48B1bf;
+    `;
+
+    const Poster = styled.img`
+    width: calc(100% - 20px);
+    height: calc(70% - 10px);
+    margin: 10px 10px 0 10px;
+    border-radius: 5px;
+    `;
+
+    const Title = styled.h5`
+    margin: 5px 0 0 0;
+    color: white;
+    `;
+
+    const Description = styled.p`
+    height: 20%;
+    color: white;
+    overflow-wrap: break-word;
+    overflow: scroll;
+    `;
+
     return(
-        <div>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} Poster`} />
-            <h5>{movie.original_title}</h5>
-            <p>{movie.overview}</p>
-        </div>
+        <Movie>
+            <Poster src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} Poster`} />
+            <Title>{movie.original_title}</Title>
+            <Description>{movie.overview}</Description>
+        </Movie>
     );
 };
 
